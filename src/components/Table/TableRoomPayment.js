a3a12bee148996e998c85b34a451df850e13026e
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2022-04-29 10:40:31 +0700

    Refactor payment

---
+++ src/components/Table/TableRoomPayment.js
@@ -0,0 +1,82 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+} from "@mui/material";
+import { useState } from "react";
+import CustomTableHead from "../../ui-component/CustomTableHead";
+import applySortFilter from "../../utils/table-sort-filter";
+import MoreMenu from "../Payment/MoreMenu";
+
+const columns = [
+  { id: "number", label: "STT", minWidth: 50, align: "center" },
+  { id: "roomName", label: "Phòng", minWidth: 150 },
+  {
+    id: "numberOfRentalDays",
+    label: "Số ngày thuê",
+    minWidth: 120,
+  },
+  {
+    id: "price",
+    label: "Đơn giá",
+    minWidth: 100,
+  },
+  {
+    id: "totalPrice",
+    label: "Thành tiền",
+    minWidth: 170,
+  },
+  {
+    id: "more",
+    label: "",
+    minWidth: 170,
+  },
+];
+
+export default function TableRoomPayment({ data, handleDelete }) {
+  const [order, setOrder] = useState("");
+  const [orderBy, setOrderBy] = useState("number");
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const sortedData = applySortFilter(data, order, orderBy);
+
+  return (
+    <TableContainer sx={{ maxHeight: 450 }}>
+      <Table stickyHeader aria-label="sticky table">
+        <CustomTableHead
+          order={order}
+          orderBy={orderBy}
+          columns={columns}
+          onRequestSort={handleRequestSort}
+        />
+        <TableBody>
+          {sortedData.map((row) => (
+            <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+              {columns.map((column) => {
+                const value = row[column.id];
+                return column.id === "more" ? (
+                  <TableCell align="center" key={row.id}>
+                    <MoreMenu room={row} handleDelete={handleDelete} />
+                  </TableCell>
+                ) : (
+                  <TableCell key={column.id} align={column.align}>
+                    {column.format && typeof value === "number"
+                      ? column.format(value)
+                      : value}
+                  </TableCell>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
