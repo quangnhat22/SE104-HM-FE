37eb713d01933dcdf472c4ae1c04aae475ff743b
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2022-05-28 10:48:16 +0700

    Update logo

--- src/layout/MainLayout/LogoSection.js
+++ src/layout/MainLayout/LogoSection.js
@@ -1,11 +1,16 @@
-import { Link } from "react-router-dom";
 import { ButtonBase } from "@mui/material";
+import { Link } from "react-router-dom";
 import config from "../../config";
-import Logo from "../../ui-component/Logo";
+import logo from "../../assets/images/logo.png";
 
 const LogoSection = () => (
-  <ButtonBase disableRipple component={Link} to={config.defaultPath}>
-    <Logo />
+  <ButtonBase
+    disableRipple
+    component={Link}
+    to={config.defaultPath}
+    sx={{ height: "50px" }}
+  >
+    <img src={logo} alt="Berry" width="100%" height="100%" />
   </ButtonBase>
 );
 
