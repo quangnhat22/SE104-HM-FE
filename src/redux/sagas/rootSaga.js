89cb26767a54c8e046fc9a3d62603013f3f2bad5
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2022-05-31 15:15:44 +0700

    User saga

--- src/redux/sagas/rootSaga.js
+++ src/redux/sagas/rootSaga.js
@@ -1,4 +1,6 @@
 import { all } from "redux-saga/effects";
+import * as UserSaga from "./UserSaga";
+import * as UserGroupSaga from "./UserGroupSaga";
 import * as RoomListSaga from "./RoomListSaga";
 import * as TypeRoomSaga from "./TypeRoomListSaga";
 import * as TypeCustomerSaga from "./TypeCustomerListSaga";
@@ -9,6 +11,14 @@ import * as RentVoucherSaga from "./RentVoucherSaga";
 
 export default function* rootSaga() {
     yield all([
+        UserSaga.followActLogin(),
+        UserSaga.followActFetchListUser(),
+        UserSaga.followActNewUser(),
+        UserSaga.followActEditUser(),
+        UserSaga.followActDeleteUser(),
+        UserSaga.followActForgotPassword(),
+        UserSaga.followActResetPassword(),
+        UserGroupSaga.followFetchListUserGroup(),
         RoomListSaga.followActFetchListRoom(),
         RoomListSaga.followActNewRoom(),
         RoomListSaga.followActEditRoom(),
