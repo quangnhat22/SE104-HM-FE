202053d65e17d51fd81b40ef844235a4a20fd1c6
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2022-06-02 13:17:56 +0700

    Delete more menu surchargerate

--- src/components/SurchargeRate/MoreMenu.js
+++ src/components/SurchargeRate/MoreMenu.js
@@ -1,27 +1,10 @@
 import { Box, IconButton, Tooltip } from "@mui/material";
-import { IconPencil, IconTrash } from "@tabler/icons";
-import { useState } from "react";
-import AlertModal from "../../ui-component/AlertModal";
+import { IconPencil } from "@tabler/icons";
 
 export default function MoreMenu({
   surchargeRate,
   handleModify,
-  handleDelete,
 }) {
-  const [openDelete, setOpenDelete] = useState(false);
-
-  const handleClose = () => {
-    setOpenDelete(false);
-  };
-
-  const handleOpen = () => {
-    setOpenDelete(true);
-  };
-
-  const handleDeleteSurchargeRate = (surchargeRate) => {
-    handleClose();
-    handleDelete(surchargeRate);
-  };
   return (
     <>
       <Box sx={{ display: "flex", justifyContent: "right" }}>
@@ -35,24 +18,7 @@ export default function MoreMenu({
             <IconPencil />
           </IconButton>
         </Tooltip>
-        <Tooltip title="Xoá">
-          <IconButton
-            variant="text"
-            color="error"
-            size="large"
-            onClick={handleOpen}
-          >
-            <IconTrash color="#F44336" />
-          </IconButton>
-        </Tooltip>
       </Box>
-      {openDelete && (
-        <AlertModal
-          content="Bạn có chắc muốn xóa tỉ lệ phụ thu này?"
-          handleClose={handleClose}
-          action={() => handleDeleteSurchargeRate(surchargeRate)}
-        />
-      )}
     </>
   );
 }
